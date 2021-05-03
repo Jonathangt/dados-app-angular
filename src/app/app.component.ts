3138d7a1c73dd3a4eea96ceeed9eb6ecaf9d3061
Jonathan Guevara <jonathanguevara27@hotmail.com>
2021-05-02 22:46:12 -0500

    first commit dados-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados-angular';
+
+  dadoIzq = '../assets/img/dice1.png';
+  dadoDer = '../assets/img/dice5.png';
+
+  numIzq = 1;
+  numDer = 2;
+
+  tirarDados():void{
+    this.numIzq = Math.round(Math.random()*5)+1;
+    this.numDer = Math.round(Math.random()*5)+1;
+
+    this.dadoIzq = `../assets/img/dice${this.numIzq}.png`
+    this.dadoDer = `../assets/img/dice${this.numDer}.png`
+
+  }
+
+
 }
